12b07722a36cfd587fc8653472543c82f77a7736
Rani Zilpelwar <rani@8thlight.com>
2025-05-29 20:50:14 -0400

    clean up unused imports

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -3,8 +3,6 @@ import { screen, waitFor } from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
 import Home from "../../pages/index";
 import { useSession } from "next-auth/react";
-import { act } from "react";
-import { useState } from "react";
 
 beforeAll(() => {
   jest.spyOn(console, "log").mockImplementation(() => {});
